f08fd3024f3ce6125997c8f165dd9472f34b4b1d
crisoalmoniga <cristian.almoniga@gmail.com>
2023-12-15 02:20:53 -0300

    Comentarios

--- Wild physics/Main.cpp
+++ Wild physics/Main.cpp
@@ -1,6 +1,6 @@
 #include "Game.h"
 
-//Me tomé la licencia de hacer una versión del shooter, faltan muchas cosas como poder reiniciar el juego y agregar animaciones, las cuales no llego a armar.
+//El juego está reciclado casi por completo del Trabajo Integrador I
 
 int main() {
    
